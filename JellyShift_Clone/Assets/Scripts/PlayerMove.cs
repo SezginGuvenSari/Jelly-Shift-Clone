767d930269f3d46184d6617f465e4eb5e8e2d38d
Güven Sarı <59062942+SezginGuvenSari@users.noreply.github.com>
2022-04-06 23:06:44 +0300

    Basic PlayerController and PlayerScale

---
+++ JellyShift_Clone/Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    private Camera cam;
+    private Rigidbody rb;
+
+    public float lerpValue;
+    public float minX, maxX;
+    public float minY, maxY;
+    private bool isGameEnded;
+    public float speed;
+    void Start()
+    {
+        cam = Camera.main;
+        rb = GetComponent<Rigidbody>();
+    }
+
+
+    void Update()
+    {
+        if (!isGameEnded)
+        {
+            Move();
+        }
+        if (Input.GetButton("Fire1"))
+        {
+            ChangeScale();
+        }
+    }
+
+
+    public void ChangeScale()
+    {
+        Vector3 mousPos = Input.mousePosition;
+        mousPos.z = 10;
+
+        Vector3 moveVec = cam.ScreenToWorldPoint(mousPos);
+
+        float x = transform.localScale.x;
+        moveVec.z = transform.localScale.z;
+
+        moveVec.y *= 2f;
+        moveVec.y = Mathf.Clamp(moveVec.y, minY, maxY);
+
+        x = 3 / moveVec.y;
+
+        moveVec.x = Mathf.Clamp(x, minX, maxX);
+
+        transform.localScale = moveVec;
+
+
+
+    }
+
+    private void Move()
+    {
+        rb.velocity = Vector3.forward * speed * Time.deltaTime;
+    }
+
+}
